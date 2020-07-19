52cee58f0c3f7db315b38972e54915154b13563e
Austin Schenk <cirrustype@icloud.com>
2020-07-18 22:28:32 -0700

    done with C# for problem 1

---
+++ Rosalind/python_village/C#/variables_arithmetic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Rosa(double a, double b){
+                double c = Math.Pow(a, 2) + Math.Pow(b, 2);
+                return c;
+            }
+            //double a2 = Math.Pow(3.00, 2.00);
+            //double b2 = Math.Pow(5.00, 2.00);
+            //double c = a2 + b2;
+            
+            double c = Rosa(815,871); //1422866, correct!
+            Console.WriteLine(c);
+        }
+    }
+}
